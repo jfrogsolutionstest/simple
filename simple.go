d8e3b32d1d2b6e5e207a02c6177c08d118c64f99
jfrogsolutionci <jfrog-solutions-ci@jfrog.com>
2019-09-11 23:20:15 +0000

    commit with no tags

---
+++ simple.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jfrogsolutionci/dog"
+)
+
+func main () {
+	fmt.Println ("Package Name: akita")
+	dog.dog.AkitaPackageName()
+	dog.dog.ColliePackageName()
+        type Context = context.Context
+        type CancelFunc = context.CancelFunc
+}
